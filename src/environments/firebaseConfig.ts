df58c710ede6c2b5da1d7b278f0764af2c0635d7
ReinierGielen <Naitok14>
2019-11-30 21:09:41 +0100

    Initial installation

--- src/environments/firebaseConfig.ts
+++ src/environments/firebaseConfig.ts
@@ -1,8 +1,9 @@
 export const FireBaseConfig = {
-  apiKey: "<YOUR_API_KEY>",
-  authDomain: "<YOUR_AUTH_DOMAIN>",
-  databaseURL: "<YOUR_DATABASE_URL>",
-  projectId: "<YOUR_PROJECT_ID>",
-  storageBucket: "<YOUR_STORAGE_BUCKETS>",
-  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>"
+  apiKey: "AIzaSyC6N2ikJ5I44LBD-JsK_McBUUjCKgZa1To",
+  authDomain: "testshop-11129.firebaseapp.com",
+  databaseURL: "https://testshop-11129.firebaseio.com",
+  projectId: "testshop-11129",
+  storageBucket: "testshop-11129.appspot.com",
+  messagingSenderId: "910824163684",
+  appId: "1:910824163684:web:1ac5dcd4f6b42fd16bd271"
 };
